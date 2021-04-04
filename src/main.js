8eb3eeb660125e08157306abf9fe76b7c88fecdd
William NORIS <w.noris@protonmail.com>
2021-04-04 16:44:09 +0200

    .

--- src/main.js
+++ src/main.js
@@ -5,7 +5,8 @@ import router from "./router";
 import store from "./store";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import VeeValidate from "vee-validate";
+//import VeeValidate from "vee-validate";
+import "vee-validate";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import {
@@ -20,9 +21,8 @@ library.add(faHome, faUser, faUserPlus, faSignInAlt, faSignOutAlt);
 
 Vue.config.productionTip = false;
 
-Vue.use(VeeValidate);
+//Vue.use(VeeValidate);
 Vue.component("font-awesome-icon", FontAwesomeIcon);
-//Vue.componenet("_vm", VeeValidate);
 
 new Vue({
     router,
